45f2e54e0da0c60216a8bfba7f6bf115aa2d3061
Disitha <disithar@gmail.com>
2025-04-05 12:24:04 +0530

    Add initial backend structure with Maven configuration and Spring Boot setup

---
+++ PetFolioBackend/petfolio-backend/src/test/java/com/petfolio/backend/PetfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petfolio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
