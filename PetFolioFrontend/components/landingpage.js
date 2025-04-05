d735d0e30bb62b051e57254e4e0d215bceddaab5
Disitha <disithar@gmail.com>
2025-04-05 14:13:36 +0530

    Implement code changes to enhance functionality and improve performance

--- PetFolioFrontend/components/landingpage.js
+++ PetFolioFrontend/components/landingpage.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { PaperProvider } from 'react-native-paper';
+import React from "react";
+import { useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { PaperProvider } from "react-native-paper";
+import { StyleSheet, Text, View, Image } from "react-native";
 export default function LandingPage() {
-    return(
-        <SafeAreaProvider>
-            <PaperProvider>
-                <div>
-                    
-                </div>
-            </PaperProvider>
-        </SafeAreaProvider>
-    )
+  return (
+    <SafeAreaProvider>
+      <PaperProvider>
+        <View>
+          <Image
+            source={require("../assets/icon.png")}
+            style={{ width: "100%", height: "100%" }}
+          />
+        </View>
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
 }
+
+const styles = StyleSheet.create({});
