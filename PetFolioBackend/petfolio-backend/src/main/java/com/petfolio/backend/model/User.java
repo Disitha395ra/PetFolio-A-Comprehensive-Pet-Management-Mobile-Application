2a9c5823eea08efb9a4e83fc2f52e5bfeadd578d
Disitha <disithar@gmail.com>
2025-04-13 15:25:55 +0530

    Refactor User class to include entity annotations and improve structure

--- PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/model/User.java
+++ PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/model/User.java
@@ -1,5 +1,30 @@
 package com.petfolio.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private String confirmpassword;
+	public User(int id, String username, String email, String password, String confirmpassword) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.confirmpassword = confirmpassword;
+	}
+	
+	
 }
