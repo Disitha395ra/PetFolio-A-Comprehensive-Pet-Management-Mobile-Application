0701276e135e9ed81126cd91f38b6d7ff176f48d
Disitha <disithar@gmail.com>
2025-04-14 18:11:45 +0530

    Update user state initialization in UserContext to an empty string for consistency

--- PetFolioFrontend/contexts/UserContext.js
+++ PetFolioFrontend/contexts/UserContext.js
@@ -1,7 +1,7 @@
 import React,{createContext, useState} from 'react';
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // Initialize user state to null
+  const [user, setUser] = useState(""); // Initialize user state to null
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
