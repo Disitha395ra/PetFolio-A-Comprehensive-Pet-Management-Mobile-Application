d59b6743d4ed0f2d92acd8e74872ea0b6bed1674
Disitha <disithar@gmail.com>
2025-04-17 10:01:31 +0530

    Refactor Home component to utilize UserContext for user state; enhance UI with welcome message and add button

--- PetFolioFrontend/pages/Home.js
+++ PetFolioFrontend/pages/Home.js
@@ -1,14 +1,72 @@
 import React, { useContext } from "react";
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
-import { UserContext } from "../contexts/UserContext"; // Importing UserContext
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { PaperProvider } from "react-native-paper";
+import { useFonts } from "expo-font";
+import { UserContext } from "../contexts/UserContext";
+import { useNavigation } from "@react-navigation/native";
+import { Avatar } from "react-native-paper";
+
 export default function Home() {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const navigation = useNavigation();
+
+  let [fontsLoaded] = useFonts({
+    "outfit-bold": require("../assets/fonts/Outfit-Bold.ttf"),
+    "outfit-regular": require("../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-light": require("../assets/fonts/Outfit-Light.ttf"),
+  });
+  if (!fontsLoaded) return null;
+
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Screen</Text>
-      <Text>Welcome, {user?.name}</Text>
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <View style={styles.container}>
+          <View style={styles.welcomemsg}>
+            <Text style={styles.welcomeText}>Hello, {user || "Guest"} 👋</Text>
+            <Text style={styles.subtitle}>Welcome to petfolio</Text>
+          </View>
+
+          <TouchableOpacity style={styles.addButton} >
+            <Avatar.Icon size={45} icon="plus" />
+          </TouchableOpacity>
+        </View>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e6ecff",
+    alignItems: "flex-start", // align items to the left
+    paddingHorizontal: 30,
+    paddingTop: 60, // create space from top
+  },
+  welcomeText: {
+    fontSize: 26,
+    fontFamily: "outfit-bold",
+    color: "#3b2a2a",
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: "outfit-light",
+    color: "#3b2a2a",
+    marginTop: 5,
+  },
+  addButton: {
+    borderRadius: 50,
+    padding: 10,
+    position: "absolute",
+    bottom: 30,
+    right: 30,
+    shadowColor: "#000",
+  },
+  welcomemsg: {
+    alignSelf: "flex-start", // make sure it's aligned to the left
+    marginBottom: 20,
+  },
+});
+
