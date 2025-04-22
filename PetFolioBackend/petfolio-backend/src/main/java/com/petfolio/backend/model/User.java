d3815d1c0cb11a847fe951cd9abd34d40a947c83
Disitha <disithar@gmail.com>
2025-04-22 19:25:07 +0530

    Refactor Pet and User models to remove unnecessary relationships and enhance encapsulation, including the removal of pets list in User and adjustments to getter/setter methods.

--- PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/model/User.java
+++ PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/model/User.java
@@ -17,10 +17,6 @@ public class User {
     private String email;
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonManagedReference
-    private List<Pet> pets;
-
     public User() {}
 
     // Getters and setters...
@@ -41,13 +37,22 @@ public class User {
         this.username = username;
     }
 
-    public List<Pet> getPets() {
-        return pets;
-    }
 
-    public void setPets(List<Pet> pets) {
-        this.pets = pets;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    // Add email, password getters/setters as needed
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    
 }
