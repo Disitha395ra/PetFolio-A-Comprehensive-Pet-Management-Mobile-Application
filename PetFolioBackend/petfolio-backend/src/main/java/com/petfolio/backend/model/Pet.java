ff94c85ca989d14f6e02bdbde6a7a3b32c285d13
Disitha <disithar@gmail.com>
2025-04-22 17:50:58 +0530

    Refactor Pet model to change petage type to String and simplify constructors and getter/setter methods for improved clarity

--- PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/model/Pet.java
+++ PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/model/Pet.java
@@ -17,97 +17,79 @@ public class Pet {
 
     private String username;
     private String petname;
-    private int petage;
+    private String petage;
     private String gender;
-    private LocalDate birthdate;
+    private String birthdate;
     private String description;
     private String photo;
-
-    // Constructors
-    public Pet() {}
-
-    public Pet(int petid, User user, String username, String petname, int petage, String gender, LocalDate birthdate, String photo, String description) {
-        this.petid = petid;
-        this.user = user;
-        this.username = username;
-        this.petname = petname;
-        this.petage = petage;
-        this.gender = gender;
-        this.birthdate = birthdate;
-        this.description = description;
-        this.photo = photo;
-    }
-
-    // Getters and Setters
-    public int getPetid() {
-        return petid;
-    }
-
-    public String getDescription() {
+	public Pet(int petid, User user, String username, String petname, String petage, String gender, String birthdate,
+			String description, String photo) {
+		super();
+		this.petid = petid;
+		this.user = user;
+		this.username = username;
+		this.petname = petname;
+		this.petage = petage;
+		this.gender = gender;
+		this.birthdate = birthdate;
+		this.description = description;
+		this.photo = photo;
+	}
+	public int getPetid() {
+		return petid;
+	}
+	public void setPetid(int petid) {
+		this.petid = petid;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPetname() {
+		return petname;
+	}
+	public void setPetname(String petname) {
+		this.petname = petname;
+	}
+	public String getPetage() {
+		return petage;
+	}
+	public void setPetage(String petage) {
+		this.petage = petage;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
 
-	public void setPetid(int petid) {
-        this.petid = petid;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPetname() {
-        return petname;
-    }
-
-    public void setPetname(String petname) {
-        this.petname = petname;
-    }
-
-    public int getPetage() {
-        return petage;
-    }
-
-    public void setPetage(int petage) {
-        this.petage = petage;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
+    // Constructors
+    
 }
