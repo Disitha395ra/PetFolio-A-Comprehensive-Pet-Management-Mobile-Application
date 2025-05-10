8cb993452fcfcb897c39262e1271b4399ff1be31
Disitha <disithar@gmail.com>
2025-05-11 01:22:44 +0530

    Add endpoint to fetch reminders by username

--- PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/controller/RemindersController.java
+++ PetFolioBackend/petfolio-backend/src/main/java/com/petfolio/backend/controller/RemindersController.java
@@ -9,9 +9,11 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.petfolio.backend.model.Reminders;
@@ -47,5 +49,31 @@ public class RemindersController {
 			e.printStackTrace();
 			return "Reminder Set Error"+e.getMessage();
 		}
+	}
+	
+	
+	@GetMapping("/getreminder")
+    public ResponseEntity<?> getPetreminders(@RequestParam String username) {
+		String dbUrl = "jdbc:mysql://localhost:3306/petfolio";
+        String dbUsername = "root";
+        String dbPassword = "";
+        
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            String sql = "SELECT * FROM reminders WHERE username=?";
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            
+            stmt.setString(4, reminder.getDate());
+            
+            stmt.executeUpdate();
+            
+            return "Reminder Add Successfully";
+        }catch(Exception e) {
+        	e.printStackTrace();
+			return "Reminders fetching Error"+e.getMessage();
+        }
+		
 	}
 }
