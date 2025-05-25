4c2a05906956c97c226a90ebb3462176f31d47fd
Disitha <disithar@gmail.com>
2025-05-25 23:05:23 +0530

    feat: add expo-font dependency and integrate custom fonts in Landscreen component

--- PetFolioFrontend/petfrontend/components/pages/Landscreen.js
+++ PetFolioFrontend/petfrontend/components/pages/Landscreen.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Text, View } from "react-native";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 export default function Landscreen(){
+    const [fontsLoaded] = useFonts({
+      Poppins_400Regular,
+      Poppins_700Bold,
+    });
+
+    if (!fontsLoaded) {
+      return null; // or <AppLoading />
+    }
     return(
         <View>
-
+            
         </View>
     )
 }
