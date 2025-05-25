6ce44b1b7d872525962ed033c683da5314abca83
Disitha <disithar@gmail.com>
2025-05-25 23:08:21 +0530

    feat: integrate Landscreen component into App.js and enhance Landscreen structure with SafeAreaProvider and PaperProvider

--- PetFolioFrontend/petfrontend/App.js
+++ PetFolioFrontend/petfrontend/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Landscreen from './components/pages/Landscreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Landscreen />
       <StatusBar style="auto" />
     </View>
   );
--- PetFolioFrontend/petfrontend/components/pages/Landscreen.js
+++ PetFolioFrontend/petfrontend/components/pages/Landscreen.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native";
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
-export default function Landscreen(){
-    const [fontsLoaded] = useFonts({
-      Poppins_400Regular,
-      Poppins_700Bold,
-    });
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { PaperProvider } from "react-native-paper";
+export default function Landscreen() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  });
 
-    if (!fontsLoaded) {
-      return null; // or <AppLoading />
-    }
-    return(
+  if (!fontsLoaded) {
+    return null; // or <AppLoading />
+  }
+  return (
+    <SafeAreaProvider>
+      <PaperProvider>
         <View>
-            
+          <Text></Text>
         </View>
-    )
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
 }
