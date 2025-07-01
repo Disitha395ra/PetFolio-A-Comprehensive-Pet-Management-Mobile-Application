b61e552246b0459e73fcd6f766bcf65d55c3ca2e
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2025-07-01 17:49:31 +0530

    add apointmnet

---
+++ PetFolioBackend/petfoliobackend/src/main/java/com/example/demo/models/Appointment.java
@@ -0,0 +1,54 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String petname;
+    private String date; // format: yyyy-MM-dd
+    private String time;
+    private String note;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPetname() {
+		return petname;
+	}
+	public void setPetname(String petname) {
+		this.petname = petname;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+    // Getters and setters
+}
